3fd14b640ff6cb70b376ccaffd51fef73eabab30
vincent <68946876+Dkrosch@users.noreply.github.com>
2021-10-22 02:02:10 +0700

    summary

--- TestGround/TestGround/ContentView.swift
+++ TestGround/TestGround/ContentView.swift
@@ -9,51 +9,81 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        HStack{
-            Rectangle()
-                .padding()
-                .background(Color.red)
-                .frame(width: 200, height: 180)
+        VStack{
+            HStack{
+                Text("Summary").font(.title) .bold().padding()
+                Spacer()
+                Text("18 Okt 2021") .padding()
+            }
+            HStack{
+                
+                VStack{
+                    Text("27").font(.system(size: 60))
+                    Text("Total Stock")
+                }
+                    .padding()
+                    .background(Color.gray)
+                    .frame(width: 200, height: 180)
+                
+                VStack{
+                    Text("Hello, world!")
+                        .padding()
+                        .background(Color.red)
+                    Text("Hello, world!")
+                        .padding()
+                        .background(Color.red)
+                    Text("Hello, world!")
+                        .padding()
+                        .background(Color.red)
+                }
+            }
+            .cornerRadius(10)
             
             VStack{
-                Text("Hello, world!")
-                    .padding()
-                    .background(Color.red)
-                Text("Hello, world!")
-                    .padding()
-                    .background(Color.red)
-                Text("Hello, world!")
-                    .padding()
-                    .background(Color.red)
-            }
-        }
-        .cornerRadius(10)
+                VStack (alignment: .leading, spacing: 6){
+                    Text("PO1")
+                    Text("PT Nama Perusahaan")
+                    Divider()
+                }
+                VStack{
+                    Text("PO1")
+                    Text("PT Nama Perusahaan")
+                    Divider()
+                }
+                VStack{
+                    Text("PO1")
+                    Text("PT Nama Perusahaan")
+                    Divider()
+                }
+                Text("View All")
+            }.background(Color.gray) .cornerRadius(10).padding()
         
-        HStack{
-            Rectangle()
-                .padding()
-                .background(Color.red)
-                .frame(width: 100, height: 100)
-                .cornerRadius(10)
             VStack{
-                Text("Jagung Rebus")
-                Divider()
-                Text("Jagungnya direbus pakai air dan dan api")
-                
-            }
-            VStack{
-                Text("4")
-                    .font(.system(size: 38))
-                Text("Karung")
-            }
+                Text("Today's Outbound") .bold()
+                HStack{
+                    padding()
+                    Text("Kecap Def")
+                    Spacer()
+                    Text("14 pcs")
+                    padding()
+                }
+                HStack{
+                    padding()
+                    Text("Kecap Def")
+                    Spacer()
+                    Text("14 pcs")
+                    padding()
+                }
+                HStack{
+                    padding()
+                    Text("Kecap Def")
+                    Spacer()
+                    Text("14 pcs")
+                    padding()
+                }
+            }.background(Color.gray) .cornerRadius(10).padding()
+        
         }
-        .background(Color.green)
-        .cornerRadius(10)
-        .padding()
-        
-        Spacer()
-        
-        
     }
 }
 
