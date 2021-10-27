76a9325f1735e9866bc5133ce2f9ac76cfcdae95
Rostadhi <54113824+Rostadhi@users.noreply.github.com>
2021-10-27 15:46:21 +0700

    nambah button

--- TestGround/Register.swift
+++ TestGround/Register.swift
@@ -12,25 +12,42 @@ struct RegisterView: View {
     
     var body: some View {
         NavigationView {
-            Form {
-                Section(header: Text("Profile")) {
-                    TextField("FirstName", text: $username)
-                    TextField("LastName", text: $username)
-                }
-                Section(header: Text("Perusahaan")) {
-                    TextField("", text: $username)
-                }
-                Section(header: Text("Email")) {
-                    TextField("", text: $username)
-                }
-                Section(header: Text("Kata Sandi")) {
-                    TextField("*******", text: $username)
-                }
-                Section(header: Text("Ulangi Kata Sandi")) {
-                    TextField("*******", text: $username)
+            VStack {
+                Form {
+                    Section(header: Text("Profile")) {
+                        TextField("FirstName", text: $username)
+                        TextField("LastName", text: $username)
+                    }
+                    Section(header: Text("Perusahaan")) {
+                        TextField("", text: $username)
+                    }
+                    Section(header: Text("Email")) {
+                        TextField("", text: $username)
+                    }
+                    Section(header: Text("Kata Sandi")) {
+                        TextField("*******", text: $username)
+                        
+                    }
+                    Section(header: Text("Ulangi Kata Sandi")) {
+                        TextField("*******", text: $username)
+                    }
+                    Section{
+                        Button (action: {
+                            print("Perform an action here...")
+                        }){
+                            Text("Buat Akun")
+                        }
+                        .frame(height: 55)
+                        .frame(minWidth: 0, maxWidth: .infinity)
+                        .background(Color.purple)
+                        .buttonStyle(PlainButtonStyle())
+                        .cornerRadius(10)
+                        .foregroundColor(.black)
+                    }
                 }
             }
             .navigationBarTitle("Register")
+            
         }
     }
 }
