373b1a29975754c6980358f10d8af2f4e09fc3f0
Rostadhi <54113824+Rostadhi@users.noreply.github.com>
2021-10-27 13:22:15 +0700

    Nambah register

---
+++ TestGround/Register.swift
@@ -0,0 +1,27 @@
+//
+//  Register.swift
+//  TestGround
+//
+//  Created by Rostadhi Akbar, M.Pd on 27/10/21.
+//
+
+import SwiftUI
+
+struct RegisterView: View {
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                Text("Test")
+            }
+            .navigationTitle("Register")
+        }
+    }
+}
+
+
+struct RegisterView_Previews: PreviewProvider {
+    static var previews: some View {
+        RegisterView()
+    }
+}
