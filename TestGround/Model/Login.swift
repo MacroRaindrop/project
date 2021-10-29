5d449177b52093dac95335351adeff18404cf83e
Rostadhi <54113824+Rostadhi@users.noreply.github.com>
2021-10-29 14:50:58 +0700

    File Login Model API

---
+++ TestGround/Model/Login.swift
@@ -0,0 +1,24 @@
+//
+//  Login.swift
+//  TestGround
+//
+//  Created by Rostadhi Akbar, M.Pd on 29/10/21.
+//
+
+import SwiftUI
+import Foundation
+
+struct Login: Hashable, Codable, Identifiable {
+    var id: UUID
+    var name: String
+    var password: String
+}
+
+class APILogin: ObservableObject{
+    //isi kodingan
+    @Published var login = [Login]()
+    
+    func loadDataAPI(completion: @escaping ([Login]) -> ()) {
+        //isi url & url session
+    }
+}
