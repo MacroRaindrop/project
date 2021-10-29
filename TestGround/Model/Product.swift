057059efc8fbba50e8cadcf9f3223059927ed96c
Rostadhi <54113824+Rostadhi@users.noreply.github.com>
2021-10-29 15:16:28 +0700

    nambah product

---
+++ TestGround/Model/Product.swift
@@ -0,0 +1,18 @@
+//
+//  Product.swift
+//  TestGround
+//
+//  Created by Rostadhi Akbar, M.Pd on 29/10/21.
+//
+
+import Foundation
+import SwiftUI
+
+struct Product: Hashable, Codable, Identifiable {
+    var id: UUID
+    var newProduct: String
+    var detailProduct: String
+    var inboundProduct: String
+    var outboundProduct: String
+    var purchaseOrder: String
+}
