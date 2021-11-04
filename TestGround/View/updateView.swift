c9f4cca47a989c4df91c87c3fa69bf3063997bbb
apicahaya <60282972+apicahaya@users.noreply.github.com>
2021-11-04 16:37:34 +0700

    adding updateView

---
+++ TestGround/View/updateView.swift
@@ -0,0 +1,20 @@
+//
+//  updateView.swift
+//  TestGround
+//
+//  Created by Agni Muhammad on 04/11/21.
+//
+
+import SwiftUI
+
+struct updateView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct updateView_Previews: PreviewProvider {
+    static var previews: some View {
+        updateView()
+    }
+}
