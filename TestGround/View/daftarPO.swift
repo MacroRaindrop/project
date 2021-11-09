a3ab4c35ee459f4f76f573179c4f56b64a08a3d7
vincent <68946876+Dkrosch@users.noreply.github.com>
2021-11-09 23:02:23 +0700

    dah kelar ui inbound

---
+++ TestGround/View/daftarPO.swift
@@ -0,0 +1,57 @@
+//
+//  daftarPO.swift
+//  TestGround
+//
+//  Created by vincent meidianto on 09/11/21.
+//
+
+import SwiftUI
+
+struct daftarPO: View {
+    var body: some View {
+        VStack{
+            Text("Daftar PO").bold().font(.title)
+            listPO()
+        }
+    }
+}
+
+struct daftarPO_Previews: PreviewProvider {
+    static var previews: some View {
+        daftarPO()
+    }
+}
+
+struct listPO: View {
+    @State var editingMode2: Bool = false
+    @State private var jumlah: String = ""
+    var body: some View {
+        NavigationView {
+            List(0 ..< 3) { item in
+                HStack {
+                    
+                    VStack (alignment: .leading, spacing: 5) {
+                        Text("kelapa kering")
+                            .font(.title3)
+                            .fontWeight(.bold)
+                        
+                        Text("PT binjai")
+                        
+                    }
+                    
+                    NavigationLink(destination: POdetail()){
+                        
+                    }
+                    
+                    
+                } .background(Color.white)
+                    .padding()
+                //                .overlay(
+                //                        RoundedRectangle(cornerRadius: 3)
+                //                            .stroke(Color.white, lineWidth: 1)
+                //                )
+            }
+        } //.scaledToFit()
+        .background(Color.primary)
+    }
+}
