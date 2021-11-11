d2e6c70ef1ad49d1b3a4a8c3ea94cc16cbc388e2
Rostadhi <54113824+Rostadhi@users.noreply.github.com>
2021-11-11 11:33:31 +0700

    nambah API Product

--- TestGround/APIService/APIProduct.swift
+++ TestGround/APIService/APIProduct.swift
@@ -8,3 +8,85 @@
 import Foundation
 import Combine
 
+class APIProduct: ObservableObject {
+    @Published var products: [Product] = []
+    
+    func addProduct() {
+        guard let url = URL(string: "https://be-raindrop-app.herokuapp.com/products") else { return }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.setValue("application/json", forHTTPHeaderField: "Accept")
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        URLSession.shared.dataTask(with: request) {(data, response, error) in
+            if let error = error {
+                print(error)
+                return
+            }
+            guard let data = data else {
+                return
+            }
+            do {
+                let products = try JSONDecoder().decode([Product].self, from: data)
+                DispatchQueue.main.async {
+                    self.products = products
+                }
+            } catch {
+                print(error)
+            }
+        }.resume()
+    }
+    
+    func getProduct() {
+        guard let url = URL(string: "https://be-raindrop-app.herokuapp.com/products?skip=0&limit=10") else { return }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        request.setValue("application/json", forHTTPHeaderField: "Accept")
+        
+        URLSession.shared.dataTask(with: request) {(data, response, error) in
+            if let error = error {
+                print(error)
+                return
+            }
+            guard let data = data else {
+                return
+            }
+            do {
+                let products = try JSONDecoder().decode([Product].self, from: data)
+                DispatchQueue.main.async {
+                    self.products = products
+                }
+            } catch {
+                print(error)
+            }
+        }.resume()
+    }
+    func updateProduct() {
+        guard let url = URL(string: "https://be-raindrop-app.herokuapp.com/products") else { return }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "PUT"
+        request.setValue("application/json", forHTTPHeaderField: "Accept")
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        URLSession.shared.dataTask(with: request) {(data, response, error) in
+            if let error = error {
+                print(error)
+                return
+            }
+            guard let data = data else {
+                return
+            }
+            do {
+                let products = try JSONDecoder().decode([Product].self, from: data)
+                DispatchQueue.main.async {
+                    self.products = products
+                }
+            } catch {
+                print(error)
+            }
+        }.resume()
+    }
+}
