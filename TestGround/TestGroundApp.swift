1278f5940bcb35fea061175d64e4a62ea02dd51a
fandikaikhsan <fandika.ikshan@gmail.com>
2021-11-11 17:47:42 +0700

    outbondview ui done (logic blm)

--- TestGround/TestGroundApp.swift
+++ TestGround/TestGroundApp.swift
@@ -12,8 +12,8 @@ struct TestGroundApp: App {
     @StateObject var loginManager = APILogin()
     var body: some Scene {
         WindowGroup {
-            LoginView(loginManager: loginManager)
-
+//            LoginView(loginManager: loginManager)
+            OutbondConfirmationView()
         }
     }
 }
