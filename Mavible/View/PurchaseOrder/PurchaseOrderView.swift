25a0baebcf5394da719ebb05eefa46ea85736b0b
jondizzy <nathanael.c2pank@gmail.com>
2021-11-18 15:45:18 +0700

    po view dan po1 view

--- Mavible/View/PurchaseOrder/PurchaseOrderView.swift
+++ Mavible/View/PurchaseOrder/PurchaseOrderView.swift
@@ -9,9 +9,62 @@ import SwiftUI
 
 struct PurchaseOrderView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            VStack {
+                Text("Buat PO")
+                    .padding()
+                    .font(.system(size: 30, weight: .bold))
+            VStack {
+                HStack {
+                    Text("PIC")
+                        .font(Font.headline.weight(.bold))
+                            Spacer()
+                    }
+                HStack{
+                    Picker(selection: .constant("pilih PIC"), label: Text("Picker")) {
+                        Text("staff1").tag(1)
+                        Text("staff2").tag(2)
+                        Text("staff3").tag(3)
+                            .frame(width: UIScreen.main.bounds.width - 40)
+                    }
+                    Spacer()
+                }
+                
+                HStack {
+                    Text("Supplier")
+                        .font(Font.headline.weight(.bold))
+                        Spacer()
+                        }
+                HStack{
+                    Picker(selection: .constant("pilih PIC"), label: Text("Picker")) {
+                        Text("supplier1").tag(1)
+                        Text("supplier2").tag(2)
+                        Text("supplier3").tag(3)
+                            .frame(width: UIScreen.main.bounds.width - 40)
+                    }
+                    
+                    Spacer()
+                }
+
+                        Text("Lanjut")
+                            .frame(maxWidth: 219, maxHeight: 20)
+                            .font(.system(size: 20))
+                            .padding()
+                            .foregroundColor(.black)
+                            .background(Color.raindropColor)
+                            .clipShape(RoundedRectangle(cornerRadius: 20))
+                            .padding()
+                    }
+                    Spacer()
+                        .frame(height: 100)
+                }
+                .padding()
+                Spacer()
+            }
+        }
     }
-}
+
+
 
 struct PurchaseOrderView_Previews: PreviewProvider {
     static var previews: some View {
