9372a32b3953a14ae3d915ee322061332efbfa79
Rostadhi <54113824+Rostadhi@users.noreply.github.com>
2021-11-24 15:46:23 +0700

    +

---
+++ Mavible/View Model/OrderViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  OrderViewModel.swift
+//  Mavible
+//
+//  Created by Rostadhi Akbar, M.Pd on 24/11/21.
+//
+
+import Foundation
+import SwiftUI
+import Combine
+import SystemConfiguration
+
+class OrderViewModel: ObservableObject {
+    
+}
