159f822ece4a47f5e813b3f2db810f13a991d0b4
apicahaya <60282972+apicahaya@users.noreply.github.com>
2021-11-25 17:26:44 +0700

    123

--- Mavible/View/Dashboard/UpdateView.swift
+++ Mavible/View/Dashboard/UpdateView.swift
@@ -9,56 +9,61 @@ import SwiftUI
 
 struct UpdateView: View {
     var body: some View {
+        NavigationView {
             ScrollView {
                 VStack(spacing: 20) {
-                    Text("")
-                    //                  NavigationLink(destination: PurchaseOrder()) {
-                    PurchaseOrder()
+                    NavigationLink(destination: PurchaseOrderView()) {
+                        PurchaseOrder()
+                        
+                    }
                     NavigationLink(destination: daftarPO() ) {
                         Inbound()
                     }
-                    //                  NavigationLink(destination: Text("Destination")) {
-                    //                        /*@PLACEHOLDER=Label Content@*/Text("Navigate")}
-                    Outbound()
+                    NavigationLink(destination: OutbondView()){
+                        Outbound()
+                    }
                 }
                 .navigationBarTitle("Pesanan")
                 .navigationBarBackButtonHidden(true)
                 .navigationBarTitleDisplayMode(.automatic)
             }
-    }
-}
-
-struct updateView_Previews: PreviewProvider {
-    static var previews: some View {
-        UpdateView()
-    }
-}
-
-struct PurchaseOrder: View {
-    var body: some View {
-        VStack(alignment: .leading, spacing: 12){
-            Text("Purchase Order")
-                .font(.title2)
-                .fontWeight(.semibold)
-                .foregroundColor(Color("ButtonFont"))
-            Text("Buat PO baru dan lihat list barang yang akan di pesan")
-                .multilineTextAlignment(.leading)
-                .opacity(0.6)
-                .foregroundColor(.buttonFont)
-            HStack{
-                Spacer()
-                Image("POicon")
-                    .resizable(capInsets: EdgeInsets())
-                    .frame(width: /*@START_MENU_TOKEN@*/135.0/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/75.0/*@END_MENU_TOKEN@*/)
-                    .opacity(1.8)
-            }
         }
-        .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
-        .frame(width: /*@START_MENU_TOKEN@*/361.0, height: 231.0/*@END_MENU_TOKEN@*/)
-        .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color("SecondaryButton")/*@END_MENU_TOKEN@*/)
-        .cornerRadius(15.0)
     }
 }
+
+    
+    
+    struct updateView_Previews: PreviewProvider {
+        static var previews: some View {
+            UpdateView()
+        }
+    }
+    
+    struct PurchaseOrder: View {
+        var body: some View {
+            VStack(alignment: .leading, spacing: 12){
+                Text("Purchase Order")
+                    .font(.title2)
+                    .fontWeight(.semibold)
+                    .foregroundColor(Color("ButtonFont"))
+                Text("Buat PO baru dan lihat list barang yang akan di pesan")
+                    .multilineTextAlignment(.leading)
+                    .opacity(0.6)
+                    .foregroundColor(.buttonFont)
+                HStack{
+                    Spacer()
+                    Image("POicon")
+                        .resizable(capInsets: EdgeInsets())
+                        .frame(width: /*@START_MENU_TOKEN@*/135.0/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/75.0/*@END_MENU_TOKEN@*/)
+                        .opacity(1.8)
+                }
+            }
+            .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            .frame(width: /*@START_MENU_TOKEN@*/361.0, height: 231.0/*@END_MENU_TOKEN@*/)
+            .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color("SecondaryButton")/*@END_MENU_TOKEN@*/)
+            .cornerRadius(15.0)
+        }
+    }
 
 struct Inbound: View {
     var body: some View {
@@ -109,3 +114,4 @@ struct Outbound: View {
         .cornerRadius(15.0)
     }
 }
+
