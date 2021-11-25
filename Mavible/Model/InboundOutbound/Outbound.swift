3a889ddd61bce989d01f50450699570587c91828
Rostadhi <54113824+Rostadhi@users.noreply.github.com>
2021-11-25 16:21:12 +0700

    +

--- Mavible/Model/InboundOutbound/Outbound.swift
+++ Mavible/Model/InboundOutbound/Outbound.swift
@@ -7,3 +7,16 @@
 
 import Foundation
 import SwiftUI
+import Combine
+
+struct OutBound: Codable {
+    var id_company: Int
+    var id_purchaseorder: Int
+    var items: [Products]
+    
+    enum CodingKeys: String, CodingKey {
+        case id_company = "id_company"
+        case id_purchaseorder = "id_purchaseorder"
+        case items = "products"
+    }
+}
---
+++ Mavible/View Model/InboundViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  InboundViewModel.swift
+//  Mavible
+//
+//  Created by Rostadhi Akbar, M.Pd on 25/11/21.
+//
+
+import Foundation
